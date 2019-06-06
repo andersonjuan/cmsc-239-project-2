b6dfb777b965bf9a2a74fc240885ea0370b1508b
James Kon <konjames@gmail.com>
2019-06-06 13:38:55 -0500

    moving function

--- src/components/general-chart.js
+++ src/components/general-chart.js
@@ -9,20 +9,6 @@ import {XYPlot,
 
 import {capitalizeFirstLetter, getStats} from './../utils.js'
 
-function dictToarray(data) {
-  return Object.keys(data).map(d => {
-    const v = data[d].total;
-    v.key = d;
-    return v;
-  })
-}
-
-// Source : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-
 // may want to try the select tag to let user to select by country
 /*
  *
@@ -111,4 +97,20 @@ export default class ButtonFilterChart extends Component {
       </div>
     );
   }
+}
+
+
+
+
+function dictToarray(data) {
+  return Object.keys(data).map(d => {
+    const v = data[d].total;
+    v.key = d;
+    return v;
+  })
+}
+
+// Source : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
